0edbccfd8590923dbc5de1d3b25052f22ed712a0
matthcol <matthias.colin@gmail.com>
2022-01-10 14:34:45 +0100

    add 1st test

---
+++ TestMovieApi/Model/MovieTest.cs
@@ -0,0 +1,37 @@
+using System;
+using WineApiRest.Model;
+using Xunit;
+
+namespace TestMovieApi
+{
+    public class MovieTest
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            var title = "Nuit Saint Georges";
+            var movie = new Movie
+            {
+                Title = title
+            };
+            Assert.NotNull(movie.Title);
+            Assert.Equal(title, movie.Title);
+            Assert.Null(movie.Id);
+        }
+
+        [Theory]
+        [InlineData(1900)]
+        [InlineData(1950)]
+        [InlineData(2022)]
+        public void TestConstructor_Color(short year) 
+        {
+            var movie = new Movie
+            {
+                Title = "Star Wars",
+                Year = year
+            };
+            Assert.Equal(movie.Year, year);
+        }
+
+    }
+}
